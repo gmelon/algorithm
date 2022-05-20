862243442597518bc54f37dea4bbf9ce3c6948b9
Gmelon <gmelon24kr@gmail.com>
2022-05-20 17:34:43 +0900

    P02_01

--- src/inflearn/array/P02_01.java
+++ src/inflearn/array/P02_01.java
@@ -1,19 +1,29 @@
 package inflearn.array;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class P02_01 {
 
-    public static void solution(int n, String intList) {
-
+    public static String solution(int n, int[] intList) {
+        List<Integer> answer = new ArrayList<>();
+        answer.add(intList[0]); // 맨 앞수는 무조건 출력
+        for (int i = 1; i < intList.length; i++) {
+            if (intList[i - 1] < intList[i]) {
+                answer.add(intList[i]);
+            }
+        }
+        return answer.toString().replaceAll("[^0-9 ]", "");
     }
 
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = Integer.parseInt(sc.nextLine());
-        String intList = sc.nextLine();
-        System.out.println(solution(n, intList));
+        int[] intList = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
+        System.out.println(solution(n, intList));
     }
 
 }
