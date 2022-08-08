7720faab6cd57365507b430648388272dcabb4e5
gmelon <hsh1769@naver.com>
2022-08-08 22:31:12 +0900

    220808

--- src/inflearn/sorting_searching/P06_06.java
+++ src/inflearn/sorting_searching/P06_06.java
@@ -1,25 +1,31 @@
 package inflearn.sorting_searching;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 public class P06_06 {
 
-    public static ArrayList<Integer> solution(int n, int[] arr){
-        ArrayList<Integer> answer=new ArrayList<>();
-        int[] tmp=arr.clone();
-        Arrays.sort(tmp);
-        for(int i=0; i<n; i++){
-            if(arr[i]!=tmp[i]) answer.add(i+1);
+    public static String solution(int n, int[] arr) {
+        int[] arrClone = arr.clone();
+        Arrays.sort(arrClone);
+
+        int idx = 0;
+        int[] answer = new int[2];
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arrClone[i] != arr[i]) {
+                answer[idx++] = i+1;
+            }
         }
-        return answer;
+        return answer[0] + " " + answer[1];
     }
-    public static void main(String[] args){
-        Scanner kb = new Scanner(System.in);
-        int n=kb.nextInt();
-        int[] arr=new int[n];
-        for(int i=0; i<n; i++) arr[i]=kb.nextInt();
-        for(int x : solution(n, arr)) System.out.print(x+" ");
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine(); // 버퍼 날리기
+        int[] arr = Stream.of(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        System.out.println(solution(n, arr));
     }
 }
