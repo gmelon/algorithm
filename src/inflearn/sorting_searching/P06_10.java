d6e76d90b494096a90f40dd454ddbc3af1d58e43
gmelon <hsh1769@naver.com>
2022-08-11 22:39:35 +0900

    220811

--- src/inflearn/sorting_searching/P06_10.java
+++ src/inflearn/sorting_searching/P06_10.java
@@ -11,11 +11,36 @@ public class P06_10 {
         // 가장 가까운 두 말의 최대 거리는?
         Arrays.sort(arr);
 
-        int lt = 1;
-        int rt = Arrays.stream(arr).max().getAsInt() - Arrays.stream(arr).min().getAsInt();
+        int lt = 1; // 두 말 사이의 최소 거리
+        int rt = Arrays.stream(arr).max().getAsInt() - Arrays.stream(arr).min().getAsInt(); // 두 말 사이의 최대 거리
 
+        int answer = 0;
 
+        while (lt <= rt) {
+            int mid = (lt + rt) / 2;
+            if (check(arr, mid) >= c) {
+                answer = mid;
+                lt = mid + 1;
+            } else {
+                rt = mid - 1;
+            }
+        }
+        return answer;
+    }
 
+    private static int check(int[] arr, int mid) {
+        int horseCount = 1;
+
+        int lastHorsePoint = arr[0];
+
+        for (int x : arr) {
+            if (x - lastHorsePoint >= mid) {
+                // 배치할 수 있는 경우
+                horseCount++;
+                lastHorsePoint = x;
+            }
+        }
+        return horseCount;
     }
 
     public static void main(String[] args) {
