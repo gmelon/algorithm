e6b41906dc0915492e493fe4ceac6a261bd700f4
gmelon <hsh1769@naver.com>
2022-09-26 15:27:22 +0900

    220926

---
+++ src/inflearn/greedy/P09_02.java
@@ -0,0 +1,55 @@
+package inflearn.greedy;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import org.jetbrains.annotations.NotNull;
+
+public class P09_02 {
+
+    public static class Meeting implements Comparable<Meeting>{
+
+        int startTime;
+        int endTime;
+
+        public Meeting(int startTime, int endTime) {
+            this.startTime = startTime;
+            this.endTime = endTime;
+        }
+
+        @Override
+        public int compareTo(@NotNull Meeting o) {
+            if (this.endTime == o.endTime) {
+                return this.startTime - o.startTime;
+            }
+            return this.endTime - o.endTime;
+        }
+    }
+
+    public static int solution(int n, List<Meeting> meetingList) {
+        int meetingCount = 0;
+
+        Collections.sort(meetingList);
+
+        int recentEndTime = 0;
+
+        for (Meeting meeting : meetingList) {
+            if (meeting.startTime >= recentEndTime) {
+                meetingCount++;
+                recentEndTime = meeting.endTime;
+            }
+        }
+        return meetingCount;
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        List<Meeting> meetingList = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            meetingList.add(new Meeting(sc.nextInt(), sc.nextInt()));
+        }
+        System.out.println(solution(n, meetingList));
+    }
+}
