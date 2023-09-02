0b2f1b4ceeae1971e38659ebaecaa321af71492b
gmelon <hsh1769@naver.com>
2023-09-02 11:19:26 +0900

    shortest path - 1주차 완료

---
+++ src/main/java/nadongbin/dp/n9/병사_배치하기.java
@@ -0,0 +1,23 @@
+package nadongbin.dp.n9;
+
+import java.util.Scanner;
+
+public class 병사_배치하기 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt(); // 병사의 수
+
+        int[] powers = new int[N];
+        for (int i = 0; i < N; i++) {
+            powers[i] = sc.nextInt();
+        }
+
+        sc.close();
+
+        // 풀이 시작
+
+
+    }
+
+}
