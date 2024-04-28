448be6bf15e92c192d2fada9d67c33f2d8005d6e
gmelon <hsh1769@naver.com>
2024-04-28 23:17:32 +0900

    240428

---
+++ src/main/java/baekjoon/p21타일_채우기.java
@@ -0,0 +1,28 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class p21타일_채우기 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        sc.close();
+
+        if (N == 1) {
+            System.out.println(1);
+            return;
+        }
+
+        int[] dp = new int[N + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= N; i++) {
+            dp[i] = dp[i - 2] % 10_007 + dp[i - 1] % 10_007;
+        }
+
+        System.out.println(dp[N] % 10_007);
+    }
+
+}
